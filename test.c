318af11a0e2fbadd63274027dbbf92defc4068d5
dongqiqi77 <2191974033@qq.com>
2020-09-14 15:35:28 +0800

    modified test.c

---
+++ test.c
@@ -0,0 +1,12 @@
+/************************else*************************************************
+> File Name: test.c
+> Author:何志远
+> Mail: 
+> Created Time: 2020年09月14日 星期一 15时31分58秒
+ ************************************************************************/
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	printf("hello world");
+	return 0;
+}
